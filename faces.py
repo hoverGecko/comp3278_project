f46bfe029f97ec69d721eb2cb3c68e39a91186d6
li-jiagen <79582350+li-jiagen@users.noreply.github.com>
2023-11-23 12:22:24 +0800

    Update faces.py

--- faces.py
+++ faces.py
@@ -8,7 +8,7 @@ from datetime import datetime
 import sys
 
 # 1 Create database connection
-myconn = mysql.connector.connect(host="localhost", user="root", passwd="xxxxxxxx", database="facerecognition")
+myconn = mysql.connector.connect(host="localhost", user="root", passwd="xxxxx", database="facerecognition")
 date = datetime.utcnow()
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
